92905ad2a8c0a783117eca5a46d6c8e8ab39ee51
Yi Zhang <yzhang.wa@gmail.com>
2013-06-17 21:19:58 +0800

    Initialized rails project ginkgo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ginkgo::Application.config.secret_token = 'ccfa50e9ffdfe0419a0564fb1ed8c7a8a483c1b1c23beb3d392623bbdfcfc87a4ec57953b5fc2433cb883794eba933814a35c2ba06a4466ad40ec8ab38da7046'
